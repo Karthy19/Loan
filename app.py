a776a02cd21ae9ecd0cabdae128453fcc5e9b778
Karthy19 <78245820+Karthy19@users.noreply.github.com>
2024-05-15 19:49:49 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask, render_template, url_for, flash, request, jsonify
+from markupsafe import Markup
+from forms import PredictionForm
+from pandas import DataFrame
+import joblib
+
+app = Flask(__name__)
+
+# Secret key protect against modifying cookies and cross-site request forgery attacks
+app.config['SECRET_KEY'] = '686ae18682ce71b6e620dfea8995d735'
+
+# Load trained model
+model = joblib.load("models/loan_model_logreg.pkl")
+
+# Home page
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+# Prediction page
+@app.route('/prediction', methods=['GET', 'POST'])
+def prediction():
+    form = PredictionForm()
+    field_list = ["SelectField", "DecimalField", "IntegerField"]
+
+    if form.validate_on_submit():
+        # convert input data as dataframe and run through pipeline
+        new_data = dict([(field.id, field.data)
+                            for field in form
+                            if field.type in field_list])
+        new_df = DataFrame([new_data])
+        new_df['Credit_History'] = new_df['Credit_History'].astype('float')
+
+        # predict
+        pred_prob = model.predict_proba(new_df)[0, 1]
+        if pred_prob > 0.5:
+            conclusion = 'APPROVED :)'
+            category = 'success'
+        else:
+            conclusion = 'REJECTED :('
+            category = 'danger'
+
+        message = Markup(
+            f'''
+            <b>Probability of loan approval</b>: {pred_prob*100:.2f}% <br>
+            <b>Conclusion</b>: Loan is {conclusion}
+            '''
+        )
+
+        flash(message, category=category)      
+        
+        # return redirect(url_for('home'))
+    return render_template('prediction.html', title='Predict', form=form, field_list=field_list)
+
+# API
+@app.route('/result', methods=['POST'])
+def result():
+
+    new_data = request.get_json(force=True)
+    new_df = DataFrame([new_data])
+    new_df['Credit_History'] = new_df['Credit_History'].astype('float')
+
+    # predict
+    pred_prob = model.predict_proba(new_df)[0, 1]
+    pred_class = 'approved' if pred_prob > 0.5 else 'rejected'
+
+    return jsonify((pred_prob, pred_class))
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
---
+++ forms.py
@@ -0,0 +1,93 @@
+from flask_wtf import FlaskForm
+from wtforms import SelectField, IntegerField, SubmitField
+from wtforms.validators import DataRequired
+from wtforms.widgets import NumberInput
+
+class PredictionForm(FlaskForm):
+
+    '''
+    Parameter for each field:
+    - id: column name that the model recognize
+    - label: text that will appear on the form
+    - description: tooltip of input field, on hover
+    '''
+
+    gender = SelectField(
+        id='Gender',
+        label='Gender',
+        description=['Gender of the applicant'],
+        choices=['Male', 'Female'])
+
+    married = SelectField(
+        id='Married',
+        label='Married',
+        description=['Marital status of the applicant'],
+        choices=['No', 'Yes'])
+
+    dependents = SelectField(
+        id='Dependents',
+        label='Number of Dependents',
+        description=['No. of people dependent on the applicant'],
+        choices=['0', '1', '2', '3+'])
+
+    education = SelectField(
+        id='Education',
+        label='Education',
+        description=['Education level of the applicant'],
+        choices=['Not Graduate', 'Graduate'])
+
+    self_employed = SelectField(
+        id='Self_Employed',
+        label='Self Employed',
+        description=['If the applicant is self-employed or not'],
+        choices=['No', 'Yes'])
+
+    applicant_income = IntegerField(
+        id='ApplicantIncome',
+        label='Applicant Income',
+        default=0,
+        description=['USD', 'The amount of income the applicant earns'],
+        validators=[DataRequired('Value must be greater than zero!')],
+        widget=NumberInput(min=0, max=999999)
+    )
+
+    coapplicant_income = IntegerField(
+        id='CoapplicantIncome',
+        label='Co-applicant Income',
+        default=0,
+        description=['USD', 'The amount of income the co-applicant earns'],
+        validators=[DataRequired('Value must be greater than zero!')],
+        widget=NumberInput(min=0, max=999999)
+    )
+
+    loan_amount = IntegerField(
+        id='LoanAmount',
+        label='Loan Amount',
+        default=0,
+        description=['USD', 'The amount of loan the applicant has requested for'],
+        validators=[DataRequired('Value must be greater than zero!')],
+        widget=NumberInput(min=0, max=999999)
+    )
+
+    loan_amount_term = IntegerField(
+        id='Loan_Amount_Term',
+        label='Loan Amount Term',
+        default=0,
+        description=['days', 'The no. of days over which the loan will be paid'],
+        validators=[DataRequired('Value must be greater than zero!')],
+        widget=NumberInput(min=0, max=9999)
+    )
+
+    credit_history = SelectField(
+        id='Credit_History',
+        label='Credit History',
+        description=['A record of a borrower\'s responsible repayment of debts'],
+        choices=[(0.0, 'Not paid'), (1.0, 'All debts paid')])
+
+    property_area = SelectField(
+        id='Property_Area',
+        label='Property Area',
+        description=['The type of location where the applicant’s property lies'],
+        choices=['Rural', 'Semiurban', 'Urban'])
+
+    submit = SubmitField(label='Predict!')
